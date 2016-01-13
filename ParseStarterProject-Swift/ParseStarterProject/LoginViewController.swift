cb68e7365835e3011c4019b37f87bee43e23c6d1
Kevin Jesse <kjesse@ucsc.edu>
2016-01-12 23:49:12 -0800

    all logins and login controllers are complete. sign in controller complete pending post result confirmation

--- ParseStarterProject-Swift/ParseStarterProject/LoginViewController.swift
+++ ParseStarterProject-Swift/ParseStarterProject/LoginViewController.swift
@@ -15,6 +15,70 @@ import UIKit
 
 import Foundation
 
+
+
+import UIKit
+
+class LoginViewController: UIViewController, PFLogInViewControllerDelegate, PFSignUpViewControllerDelegate {
+    
+    let loginViewController = PFLogInViewController()
+    var backgroundImage : UIImageView!;
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+    
+        if (PFUser.currentUser() == nil) {
+            loginViewController.delegate = self
+            loginViewController.fields = [.UsernameAndPassword , .LogInButton, .PasswordForgotten, .SignUpButton,  .Facebook, .Twitter]
+            loginViewController.emailAsUsername = true
+            loginViewController.signUpController = SignUpViewController()
+            
+            let logo = UILabel()
+            logo.text = "Quilts For You"
+            logo.textColor = UIColor.whiteColor()
+            logo.font = UIFont(name: "billabong", size: 70)
+            logo.shadowColor = UIColor.lightGrayColor()
+            logo.shadowOffset = CGSizeMake(2, 2)
+            
+            loginViewController.logInView?.logo = logo
+            loginViewController.logInView?.emailAsUsername = true
+            
+            customizeButton(loginViewController.logInView?.signUpButton!)
+            customizeButton(loginViewController.logInView?.logInButton!)
+            
+            loginViewController.logInView?.logInButton?.backgroundColor = UIColor(red: 52/255, green: 191/255, blue: 73/255, alpha: 0.5)
+            loginViewController.logInView?.usernameField?.backgroundColor = UIColor(white: 1, alpha: 0.8)
+            loginViewController.logInView?.passwordField?.backgroundColor = UIColor(white: 1, alpha: 0.8)
+            loginViewController.logInView?.signUpButton?.backgroundColor = UIColor(white: 1, alpha: 0.5)
+            loginViewController.logInView?.passwordForgottenButton?.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+            
+            //background image
+            UIGraphicsBeginImageContext(self.view.frame.size)
+            UIImage(named: "bg.png")?.drawInRect(self.view.bounds)
+            let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            loginViewController.logInView!.backgroundColor = UIColor(patternImage: image)
+            
+            self.presentViewController(loginViewController, animated: false, completion: nil)
+        } else {
+            //presentLoggedInAlert()
+        }
+    }
+    
+    func logInViewController(logInController: PFLogInViewController, didLogInUser user: PFUser) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+        print("successful login")
+        //presentLoggedInAlert()
+    }
+    
+
+
+
+    func presentLoggedInAlert() {
+        //print("done")
+    }
+}
+
 func customizeButton(button: UIButton!) {
     button.setBackgroundImage(nil, forState: .Normal)
     button.backgroundColor = UIColor.clearColor()
@@ -23,109 +87,39 @@ func customizeButton(button: UIButton!) {
     button.layer.borderColor = UIColor.whiteColor().CGColor
 }
 
-
-class LoginViewController : PFLogInViewController, PFLogInViewControllerDelegate {
-    
-    var backgroundImage : UIImageView!;
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // set our custom background image
-        backgroundImage = UIImageView(image: UIImage(named: "bg"))
-        backgroundImage.contentMode = UIViewContentMode.ScaleAspectFill
-        logInView!.insertSubview(backgroundImage, atIndex: 0)
-        
-        // remove the parse Logo
-        let logo = UILabel()
-        logo.text = "Quilts For You"
-        logo.textColor = UIColor.whiteColor()
-        logo.font = UIFont(name: "billabong", size: 70)
-        logo.shadowColor = UIColor.lightGrayColor()
-        logo.shadowOffset = CGSizeMake(2, 2)
-        logInView?.logo = logo
-
-        self.fields = [.UsernameAndPassword, .LogInButton, .PasswordForgotten, .SignUpButton, .Facebook, .Twitter]
-        self.emailAsUsername = true
-        self.logInView?.facebookButton?.addTarget(self, action: "_loginWithFacebook", forControlEvents: .TouchUpInside);
-        self.logInView?.twitterButton?.addTarget(self, action: "_loginWithTwitter", forControlEvents: .TouchUpInside)
-        
-        customizeButton(logInView?.signUpButton!)
-        customizeButton(logInView?.logInButton!)
-        
-        logInView?.logInButton?.backgroundColor = UIColor(red: 52/255, green: 191/255, blue: 73/255, alpha: 0.5)
-        logInView?.usernameField?.backgroundColor = UIColor(white: 1, alpha: 0.8)
-        logInView?.passwordField?.backgroundColor = UIColor(white: 1, alpha: 0.8)
-        logInView?.signUpButton?.backgroundColor = UIColor(white: 1, alpha: 0.5)
-        logInView?.passwordForgottenButton?.setTitleColor(UIColor.whiteColor(), forState: .Normal)
-
-        // use our custom SignUpViewController
-        self.signUpController = SignUpViewController()
-        self.signUpController?.delegate = SignUpViewController()
-    }
-
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        // stretch background image to fill screen
-        backgroundImage.frame = CGRectMake( 0,  0,  logInView!.frame.width,  logInView!.frame.height)
-        
-        // position logo at top with larger frame
-        logInView!.logo!.sizeToFit()
-        let logoFrame = logInView!.logo!.frame
-        logInView!.logo!.frame = CGRectMake(logoFrame.origin.x, logInView!.usernameField!.frame.origin.y - logoFrame.height - 40, logInView!.frame.width,  logoFrame.height)
-    }
-
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
-        print(PFUser.currentUser()?.username)
-        if (PFUser.currentUser()?.username != nil) {
-            self.performSegueWithIdentifier("loginSuccess", sender: self)
-        }
-    }
-    
-    func logInViewController(logInController: PFLogInViewController, didLogInUser user: PFUser) {
-        print("successful login")
-    }
-
-    func signUpViewController(signUpController: PFSignUpViewController, didSignUpUser user: PFUser){
-        print("successful login")
-        self.dismissViewControllerAnimated(true, completion: nil)
-        
-    }
-}
-
-
 class SignUpViewController : PFSignUpViewController, PFSignUpViewControllerDelegate{
     
+    //let SignUpViewController = PFSignUpViewController()
     var backgroundImage : UIImageView!;
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        signUpView?.delegate = self
         
         // set our custom background image
         backgroundImage = UIImageView(image: UIImage(named: "bg"))
         backgroundImage.contentMode = UIViewContentMode.ScaleAspectFill
         signUpView!.insertSubview(backgroundImage, atIndex: 0)
-        
+
         // remove the parse Logo
         let logo = UILabel()
-        logo.text = "Quilts For You"
+        logo.text = "Quilts For Kevin"
         logo.textColor = UIColor.whiteColor()
         logo.font = UIFont(name: "billabong", size: 70)
         logo.shadowColor = UIColor.lightGrayColor()
         logo.shadowOffset = CGSizeMake(2, 2)
-        signUpView?.logo = logo
-
+        self.signUpView?.logo = logo
+        
         // change dismiss button to say 'Already signed up?'
-        signUpView?.dismissButton!.setTitle("Already signed up?", forState: .Normal)
+        self.signUpView?.dismissButton!.setTitle("Already signed up?", forState: .Normal)
         signUpView?.dismissButton!.setImage(nil, forState: .Normal)
         
         self.emailAsUsername = true
-        signUpView?.usernameField?.backgroundColor = UIColor(white: 1, alpha: 0.8)
-        signUpView?.passwordField?.backgroundColor = UIColor(white: 1, alpha: 0.8)
+        self.signUpView?.usernameField?.backgroundColor = UIColor(white: 1, alpha: 0.8)
+        self.signUpView?.passwordField?.backgroundColor = UIColor(white: 1, alpha: 0.8)
         customizeButton(signUpView?.signUpButton!)
-        signUpView?.signUpButton!.backgroundColor = UIColor(red: 1/255, green: 159/255, blue: 233/255, alpha: 0.5)
-    
-        // modify the present tranisition to be a flip instead
+        self.signUpView?.signUpButton!.backgroundColor = UIColor(red: 1/255, green: 159/255, blue: 233/255, alpha: 0.5)
+        
         self.modalTransitionStyle = UIModalTransitionStyle.FlipHorizontal
     }
     
@@ -137,21 +131,15 @@ class SignUpViewController : PFSignUpViewController, PFSignUpViewControllerDeleg
         // position logo at top with larger frame
         signUpView!.logo!.sizeToFit()
         let logoFrame = signUpView!.logo!.frame
-        signUpView!.logo!.frame = CGRectMake(logoFrame.origin.x, signUpView!.usernameField!.frame.origin.y - logoFrame.height - 40, signUpView!.frame.width,  logoFrame.height)
+        signUpView!.logo!.frame = CGRectMake(logoFrame.origin.x, signUpView!.usernameField!.frame.origin.y - logoFrame.height - 16, signUpView!.frame.width,  logoFrame.height)
         
         // re-layout out dismiss button to be below sign
         let dismissButtonFrame = signUpView!.dismissButton!.frame
-        signUpView?.dismissButton!.frame = CGRectMake(0, signUpView!.signUpButton!.frame.origin.y + signUpView!.signUpButton!.frame.height + 40.0,  signUpView!.frame.width,  dismissButtonFrame.height)
+        signUpView?.dismissButton!.frame = CGRectMake(0, signUpView!.signUpButton!.frame.origin.y + signUpView!.signUpButton!.frame.height + 16.0,  signUpView!.frame.width,  dismissButtonFrame.height)
     }
     
-//    func logInViewController(logInController: PFLogInViewController, didLogInUser user: PFUser) {
-//        self.dismissViewControllerAnimated(true, completion: nil)
-//        print("successful login")
-//    }
-//    
     func signUpViewController(signUpController: PFSignUpViewController, didSignUpUser user: PFUser){
-        print("successful login")
+        print("successful signup")
         self.dismissViewControllerAnimated(true, completion: nil)
-        
     }
 }
